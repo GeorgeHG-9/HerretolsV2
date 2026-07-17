49de8f5ffca778efb6eb70d2bfdd2e240c086ff9
George B <11hgeorge@gmail.com>
2026-07-17 12:26:29 -0500

    update3

--- app/src/main/java/com/example/herretols/config/secrets.example.kt
+++ app/src/main/java/com/example/herretols/config/secrets.example.kt
@@ -1,5 +1,6 @@
 package com.example.herretols.config
 // 1. duplicar este archivo en este directorio
+// 2. cambiar el nombre de ese archivo de (secrets.example) => (Secrets)
 // 2. cambiar de (object secrets) => (object Secrets)
 // 3. iniciar sesion en https://aistudio.google.com/prompts/new_chat para crear api
 // 4. colocar la GEMINI_API_KEY entre las comillas
