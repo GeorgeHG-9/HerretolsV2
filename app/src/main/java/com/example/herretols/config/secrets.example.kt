9527793fd31eb7d4a6921c43f75294b0b2853b8a
George B <11hgeorge@gmail.com>
2026-07-17 12:20:53 -0500

    update

--- app/src/main/java/com/example/herretols/config/secrets.example.kt
+++ app/src/main/java/com/example/herretols/config/secrets.example.kt
@@ -1,5 +1,7 @@
 package com.example.herretols.config
-
+// 1.duplicar este archivo en este directorio
+// 2. cambiar de (object secrets) => (object Secrets)
+// 3. colocar la GEMINI_API_KEY entre las comillas
 object secrets {
     //const val GEMINI_API_KEY = "PON_TU_CLAVE_AQUI"
 }
